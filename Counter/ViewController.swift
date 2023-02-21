cf0c2214dd8c835655254a379acf4ee4657c5c17
TemerovaJaa <Snezhana.temerova@gmail.com>
2023-02-21 20:37:39 +0100

    Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+    @IBOutlet weak var text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        func count (number: Int ) {
+            var number = 0
+            repeat {
+                number += 1
+            } while number >= 42
+            print ("Значение счетчика /(number)")
+        }
+               }
+    @IBAction func button(_ sender: Any) {
+        print("Нажать")
     }
-
-
 }
-
